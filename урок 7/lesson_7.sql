d846099c6ea26af4243c69fd5f7c268cec8e894a
ss2576 <ss2576@mail.ru>
2019-11-09 17:42:04 +0300

    ДЗ к видеоуроку 7

---
+++ урок 7/lesson_7.sql
@@ -0,0 +1,27 @@
+﻿-- Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+
+use shop;
+SELECT id, name FROM users WHERE id IN (SELECT DISTINCT user_id FROM orders);
+
+
+-- Выведите список товаров products и разделов catalogs, который соответствует товару..
+
+use shop;
+SELECT catalogs.name, products.name, products.price FROM products JOIN catalogs ON products.catalog_id = catalogs.id ORDER BY catalogs.name ;
+
+
+
+-- (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
+-- Поля from, to и label содержат английские названия городов, поле name — русское.
+-- Выведите список рейсов flights с русскими названиями городов.
+use test;
+SELECT flights.id AS номер_рейса ,cities_from.name AS откуда, cities_to.name AS куда FROM flights 
+JOIN cities AS cities_from ON flights.`from` = cities_from.lable
+JOIN cities AS cities_to   ON flights.`to`   = cities_to.lable
+ORDER BY номер_рейса;
+ 
+
+
+
+
+ 
