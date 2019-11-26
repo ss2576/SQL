6cc4e80a809b6d152396e02b0d9727b9ad6290a4
ss2576 <ss2576@mail.ru>
2019-11-26 10:19:20 +0300

    добавлен триггер

--- проект БД/requests.sql
+++ проект БД/requests.sql
@@ -5,6 +5,8 @@ SELECT concat(lastname,'   ',firstname,'   ', middlename) , name_posts AS дол
 FROM user_profiles, user_posts, filials where user_posts.id_posts = user_profiles.post AND filials.id_filials = user_profiles.filial;
 
 
+
+
 -- на основании 4-х таблиц patient_profiles , user_profiles , medical_cards ,user_posts  
 -- вывести ФИО пациента и должность врача и ФИО врача ,который сделал запись в медицинской карте пациента.
 USE medical_clinic;
@@ -18,7 +20,47 @@ JOIN user_posts AS up ON u.post = up.id_posts
 ORDER BY CONCAT (p.lastname,'  ',p.firstname,'  ', p.middlename);
 
 
+
 -- представление (VIEW) --
 CREATE OR REPLACE  VIEW pat AS SELECT * FROM patient_profiles ORDER BY lastname;
 SHOW TABLES;
 SELECT * FROM pat;
+
+
+
+
+-- Создание таблицы logs типа Archive. При каждом создании записи в таблицах user_profiles, 
+-- patient_profiles и schedule в таблицу logs помещается время и дата создания записи, название таблицы, --
+-- идентификатор первичного ключа и содержимое поля lastname.
+DROP TABLE IF EXISTS `logs`;
+
+CREATE TABLE `logs` (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  created_at datetime DEFAULT current_timestamp(),
+  name varchar(100) NOT NULL,
+  table_id int(10) NOT NULL,
+  contet_name varchar(255)
+) ENGINE=ARCHIVE;
+
+DROP TRIGGER IF EXISTS write_user_profiles_log_insert;
+DELIMITER //
+CREATE TRIGGER `write_user_profiles_log_insert` AFTER INSERT ON `user_profiles` FOR EACH ROW BEGIN
+	INSERT INTO `logs` (name, table_id, contet_name)
+	VALUES ('врачи', (SELECT NEW.id_user), (SELECT NEW.lastname));
+END//
+
+DROP TRIGGER IF EXISTS write_patient_profiles_log_insert;
+DELIMITER // 
+CREATE TRIGGER `write_patient_profiles_log_insert` AFTER INSERT ON `patient_profiles` FOR EACH ROW BEGIN
+	INSERT INTO `logs` (name, table_id, contet_name)
+	VALUES ('пациенты', (SELECT NEW.id_profiles), (SELECT NEW.lastname));
+END//
+
+DROP TRIGGER IF EXISTS write_shedule_log_insert;
+DELIMITER // 
+CREATE TRIGGER `write_schedule_log_insert` AFTER INSERT ON `schedule` FOR EACH ROW BEGIN
+	INSERT INTO `logs` (name, table_id, contet_name)
+	VALUES ('расписание', (SELECT NEW.id_schedule), (SELECT NEW.patient_schedule));
+END//
+
+SELECT * FROM logs
